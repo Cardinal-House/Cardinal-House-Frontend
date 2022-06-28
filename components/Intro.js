d2937faa070f20712a8f46908c5134f5b5140a11
Cole Medin <medin169@umn.edu>
2022-06-28 17:02:22 -0500

    Updated intro background to look good on mobile

--- components/Intro.js
+++ components/Intro.js
@@ -5,8 +5,8 @@ import styles from '../styles/Home.module.css';
 
 export default function Intro(props) {  
   return (
-    <Grid container justify="center" className={clsx(styles.introGrid, styles.introBackground)}>
-        <Grid item xs={6} className={styles.introText}>
+    <Grid container className={clsx(styles.introGrid, styles.introBackground)}>
+        <Grid item lg={6} md={6} sm={6} xs={12} className={styles.introText}>
         <Typography variant="h3" className={clsx(props.useDarkTheme ? styles.introTextTypographyDark : styles.introTextTypographyLight, "mb-4", styles.headerText)}>
         <b className={props.useDarkTheme ? styles.boldTextDark : styles.boldTextLight}>Community</b> for Crypto <b className={props.useDarkTheme ? styles.boldTextDark : styles.boldTextLight}>Investors </b>
         and Project <b className={props.useDarkTheme ? styles.boldTextDark : styles.boldTextLight}>Owners</b>
@@ -15,7 +15,7 @@ export default function Intro(props) {
             Cardinal House is a blockchain ecosystem with a focus on education, community, and
             crypto services that benefit everyone involved
         </Typography>
-        <Grid container justifyContent="left">
+        <Grid container justifyContent="left" className={styles.introBtnGrid}>
             <div className={styles.floated}>
               <Button href="/team" target="_blank" rel="noreferrer" size="small" variant="contained" color="primary" 
                 className={props.useDarkTheme ? styles.teamBtnDark : styles.teamBtnLight}>
