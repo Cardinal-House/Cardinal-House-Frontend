eb60436bdda810abd4b66223becc4c2b76355d00
Cole Medin <medin169@umn.edu>
2022-07-26 16:23:06 -0500

    Added Clarity

--- pages/_app.js
+++ pages/_app.js
@@ -95,6 +95,12 @@ function MyApp({ Component, pageProps }) {
     gtag('js', new Date());
   
     gtag('config', 'G-TJSPRD6Y1T');
+
+    (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "cxz5yo9w1v");
   }, [])
 
   useEffect(() => {
